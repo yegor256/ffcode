e5193f2330c18897a0d00ca51dafd62b9a0299df
Yegor Bugayenko <yegor256@gmail.com>
2022-09-12 12:47:03 +0300

    #9 more fields

--- build.lua
+++ build.lua
@@ -8,5 +8,9 @@ uploadconfig = {
   note = "Bug fixes",
   announcement = "Some non-critical bug fixes",
   ctanPath = "/macros/latex/contrib/ffcode",
-  bugtracker = "https://github.com/yegor256/ffcode/issues"
+  bugtracker = "https://github.com/yegor256/ffcode/issues",
+  home = "https://github.com/yegor256/ffcode",
+  description = "This LaTeX package helps you write source code in your academic papers and make sure it looks neat. It uses minted and tcolorbox, configuring them the right way, to make sure code fragments and code blocks look nicer.",
+  development = "https://github.com/yegor256/ffcode",
+  license = "MIT License"
 }
