bd797b09401bd2d1f5be10398cc68f254190e5e9
Yegor Bugayenko <yegor256@gmail.com>
2022-09-12 12:31:07 +0300

    #9 ctanPath

--- build.lua
+++ build.lua
@@ -1,10 +1,12 @@
 module = "ffcode"
 uploadconfig = {
- pkg = "ffcode",
- version = "0.0.0",
- author = "Yegor Bugayenko",
- uploader = "Yegor Bugayenko",
- email = "yegor256@gmail.com",
- note = "Bug fixes",
- announcement = "Some non-critical bug fixes"
+  pkg = "ffcode",
+  version = "0.0.0",
+  author = "Yegor Bugayenko",
+  uploader = "Yegor Bugayenko",
+  email = "yegor256@gmail.com",
+  note = "Bug fixes",
+  announcement = "Some non-critical bug fixes",
+  ctanPath = "/macros/latex/contrib/ffcode",
+  bugtracker = "https://github.com/yegor256/ffcode/issues"
 }
