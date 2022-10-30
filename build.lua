8f88dc4a2c6203209d84e739bb63d0497c4e2192
rultor <me@rultor.com>
2022-10-30 07:10:05 +0000

    version set to 0.5.1

--- build.lua
+++ build.lua
@@ -8,7 +8,7 @@ checkengines = {"pdftex", "luatex", "xetex"}
 
 uploadconfig = {
   pkg = "ffcode",
-  version = "0.0.0",
+  version = "0.5.1",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
